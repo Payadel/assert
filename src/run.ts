27ae655c441527fc3e628c8a72371e2c2b2f6c96
Hamid Molareza <kitm.hamid@gmail.com>
2023-04-24 20:32:50 +0330

    fix: rename `errors` output to `messages`

--- src/run.ts
+++ src/run.ts
@@ -3,24 +3,22 @@ import * as core from "@actions/core";
 import {getInputs} from "./Inputs";
 import {controller} from "./controller";
 
-const run = () => {
-    getInputs()
-        .then(controller)
-        .then(outputs => {
-            core.setOutput("success", outputs.success);
-            core.setOutput("errors", outputs.messages);
+const run = () => getInputs()
+    .then(controller)
+    .then(outputs => {
+        core.setOutput("success", outputs.success);
+        core.setOutput("messages", outputs.messages);
 
-            if (!outputs.success)
-                core.setFailed("")
-        })
-        .then(() => core.info("Operation completed successfully."))
-        .catch(error => {
-            core.error("Operation failed.")
-            if (error instanceof Error)
-                core.setFailed(error.message);
-            else
-                core.setFailed(error)
-        });
-}
+        if (!outputs.success)
+            core.setFailed("")
+    })
+    .then(() => core.info("Operation completed successfully."))
+    .catch(error => {
+        core.error("Operation failed.")
+        if (error instanceof Error)
+            core.setFailed(error.message);
+        else
+            core.setFailed(error)
+    });
 
 export default run;
