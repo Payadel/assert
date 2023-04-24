7bd3fc6e8ff601b57b106ab5e900d89c1aafd2a2
Hamid Molareza <kitm.hamid@gmail.com>
2023-04-24 20:48:58 +0330

    refactor: create utility.ts for tests

--- __tests__/inputs.test.ts
+++ __tests__/inputs.test.ts
@@ -1,51 +1,13 @@
 import { getInputs, IInputs } from "../src/Inputs";
 import * as core from "@actions/core";
-
-const yamlInput = `
-      - name: Test1
-        type: equals
-        expected: 5
-        actual: 5
-      - name: Test2
-        type: not-equals
-        expected: hello
-        actual: world
-    `;
-
-function mockGetBooleanInput(
-  name: string,
-  failFast: boolean | any,
-  verbose: boolean | any,
-  default_value: boolean = false
-) {
-  switch (name) {
-    case "fail-fast":
-      return failFast;
-    case "verbose":
-      return verbose;
-    default:
-      return default_value;
-  }
-}
-
-function mockGetInput(
-  name: string,
-  inputs: string,
-  default_value: string = ""
-) {
-  switch (name) {
-    case "inputs":
-      return inputs;
-    default:
-      return default_value;
-  }
-}
+// @ts-ignore
+import {mockGetBooleanInput, mockGetInput, validInputYaml} from "./utility";
 
 describe("getInputs", () => {
   it("should parse YAML input and return IInputs", async () => {
     jest
       .spyOn(core, "getInput")
-      .mockImplementation((name: string) => mockGetInput(name, yamlInput));
+      .mockImplementation((name: string) => mockGetInput(name, validInputYaml));
     jest
       .spyOn(core, "getBooleanInput")
       .mockImplementation((name: string) =>
---
+++ __tests__/utility.ts
@@ -0,0 +1,47 @@
+export const validInputYaml = `
+      - name: Test1
+        type: equals
+        expected: 5
+        actual: 5
+      - name: Test2
+        type: not-equals
+        expected: hello
+        actual: world
+    `;
+
+export function mockGetBooleanInput(
+    name: string,
+    failFast: boolean | any,
+    verbose: boolean | any,
+    default_value: boolean = false
+) {
+    switch (name) {
+        case "fail-fast":
+            return failFast;
+        case "verbose":
+            return verbose;
+        default:
+            return default_value;
+    }
+}
+
+export function mockGetInput(
+    name: string,
+    inputs: string,
+    default_value: string = ""
+) {
+    switch (name) {
+        case "inputs":
+            return inputs;
+        default:
+            return default_value;
+    }
+}
+
+export function mockSetOutput(
+    name: string,
+    value: any,
+    output: {[key: string]: any}
+) {
+    output[name] = value
+}
